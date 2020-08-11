3ad98178ccd7a538844b383e095b0e8790ea88a5
sabariask <sabariask1307@gmail.com>
2020-08-11 08:51:46 +0530

    login page for student and admin

---
+++ login/login.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class login extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("studid");
+		String pw=request.getParameter("dob");
+		
+		// Connect to mysql and verify username password
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		 // loads driver
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "root"); // gets a new connection
+ 
+		PreparedStatement ps = c.prepareStatement("select studid,dob from signup where studid=? and dob=?");
+		ps.setString(1, un);
+		ps.setString(2, pw);
+ 
+		ResultSet rs = ps.executeQuery();
+ 
+		while (rs.next()) {
+			response.sendRedirect("success.html");
+			return;
+		}
+		response.sendRedirect("error.html");
+		return;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
